a9f2d46ce56dd32be9786c63210093a42a3e4c41
Jonas Wunderlich <jonas.wunderlich@zalari.de>
2016-01-25 12:59:39 +0100

    added basic-functionality for todo-controller

--- app/scripts/controllers/todo.js
+++ app/scripts/controllers/todo.js
@@ -8,8 +8,64 @@
  * Controller of the toDoApp
  */
 angular.module('toDoApp')
-  .controller('ToDoCtrl', function ($scope, toDoList) {
+  .controller('ToDoCtrl', function ($scope, toDoList, $log) {
 
     $scope.todos = toDoList;
+    $scope.newToDoItem = {
+      state: 0,
+      text: '',
+      type: 'A'
+    };
+
+    /**
+     * @ngdoc method
+     * @name changeState
+     * @methodOf toDoApp.ToDoCtrl
+     * @description Changes The State of a ToDo-Item
+     * @param todo
+     */
+    $scope.changeState = function(todo) {
+      todo.state = (todo.state + 1) % 3;
+    };
+
+    /**
+     * @ngdoc method
+     * @name deleteToDo
+     * @methodOf toDoApp.ToDoCtrl
+     * @description Deletes ToDo-Item
+     * @param todo
+     */
+    $scope.deleteToDo = function(todo) {
+      var index = $scope.todos.indexOf(todo);
+      $scope.todos.splice(index, 1);
+    };
+
+    /**
+     * @ngdoc method
+     * @name createNewToDoItem
+     * @methodOf toDoApp.ToDoCtrl
+     * @description Creates a new ToDo-Item
+     * @param todo
+     */
+    $scope.createNewToDoItem = function(todo) {
+      $log.debug(todo);
+      $scope.todos.push(todo);
+      $scope.newToDoItem = {
+        state: 0,
+        text: '',
+        type: 'A'
+      };
+    };
+
+    /**
+     * @ngdoc method
+     * @name editTodo
+     * @methodOf toDoApp.ToDoCtrl
+     * @description Puts ToDo-Item in Edit-Mode
+     * @param todo
+     */
+    $scope.editTodo = function(todo) {
+      $log.debug(todo.text + " in edit mode");
+    }
 
   });
